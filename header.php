f118bc1c599b81388759fb9c9f9f253cc944e4f3
Slava Pascaru <pascaruv@gmail.com>
2021-03-30 18:25:31 +0300

    added BootStrap

--- header.php
+++ header.php
@@ -1,25 +1,30 @@
 <?php
-	/**
-	 * Header File
-	 *
-	 * @package Aquila
-	 */
-	if ( ! defined( 'ABSPATH' ) ) {
-		exit;
-	}
+
+/**
+ * Header File
+ *
+ * @package Aquila
+ */
+if (!defined('ABSPATH')) {
+	exit;
+}
 ?>
 <!doctype html>
 <html lang="<?php language_attributes(); ?>">
+
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php wp_title(); ?></title>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class();?>>
-<?php wp_body_open(); ?>
-<header>
-    Header
-</header>
+
+<body <?php body_class(); ?>>
+	<?php
+	if (function_exists('wp_body_open')) {
+		wp_body_open();
+	} ?>
+	<header>
+		Header
+	</header>
